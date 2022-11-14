2618a86cab8f4463268487b506e25c71425474a5
xqmeng <xiangqiao.meng@qq.com>
2022-11-14 13:07:57 +0800

    fix bugs in pre and post process

--- utils/transforms.py
+++ utils/transforms.py
@@ -15,13 +15,15 @@ class Preprocess(object):
         :return     torch.Tensor    [3 x h x w] (3 channel: L, a, b)
         """
         img_arr = np.array(image).astype(np.float32)
-        h, w, c = img_arr.shape
+        h, w, _ = img_arr.shape
         for i in range(h):
             for j in range(w):
                 img_arr[i][j] = utils.rgb2lab(img_arr[i][j][0], img_arr[i][j][1], img_arr[i][j][2])
         img_arr[:, :, 0] = img_arr[:, :, 0] / 100
         img_arr[:, :, 1] = (img_arr[:, :, 1] + 128) / 255
         img_arr[:, :, 2] = (img_arr[:, :, 2] + 128) / 255
+        img_arr = img_arr.swapaxes(1, 2)
+        img_arr = img_arr.swapaxes(0, 1)
         return torch.from_numpy(img_arr)
 
 
@@ -34,6 +36,8 @@ class Postprocess(object):
         :param[in]  image   torch.Tensor    [3 x h x w] (3 channel: L, a, b)
         """
         img_arr = image.cpu().detach().numpy()
+        img_arr = img_arr.swapaxes(0, 1)
+        img_arr = img_arr.swapaxes(1, 2)
         img_arr[:, :, 0] = img_arr[:, :, 0] * 100
         img_arr[:, :, 1] = img_arr[:, :, 1] * 255 - 128
         img_arr[:, :, 2] = img_arr[:, :, 2] * 255 - 128
